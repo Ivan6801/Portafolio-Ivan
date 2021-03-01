9e5d1dffa235dd2d968519a0911a466f60280212
Ivan6801 <60527338+Ivan6801@users.noreply.github.com>
2021-02-28 16:15:18 -0800

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -1,9 +1,9 @@
-var ivan = {
+const ivan = {
     nombre: 'Iván Alexander',
     apellidoPadre: 'González',
     apellidoMadre: 'González',
     edad: 20,
-    altura: 1.65,
+    altura: 1.68,
     fecha: '19 de Octubre de 1999',
     nacionalidad: 'México',
     ubicacion: 'Tijuana',
@@ -21,6 +21,7 @@ function imprimirDatos(persona) {
     document.getElementById('correo').innerHTML = (`${persona.correo}`)
     document.getElementById('nacionalidad').innerHTML = (`${persona.nacionalidad}`)
 }
+imprimirDatos(ivan)
 
 // var cumpleanos = persona => ({
 //     ...persona,
@@ -31,6 +32,15 @@ function imprimirDatos(persona) {
 // cumpleanos(ivan)
 
 
+let lista = document.getElementsByClassName('project');
 
-imprimirDatos(ivan)
+function impirmirMostrar() {
+    document.getElementById('article').innerHTML = (`${lista.length}`)
+    console.log('Articles:',lista.length)
+    // document.write(lista.length)
+}
+
+impirmirMostrar()
+
+
 
